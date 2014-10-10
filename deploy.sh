636972d93eb0fe83f1c4bb584002c577fece6e8e
Inscure <drimer.eco@gmail.com>
2014-10-11 00:16:58 +0200

    Showing the messages for executed commands

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+target_branch=$1
+
+status=`execute "git checkout $target_branch"`
+echo $status
+
+status=`execute "git merge origin/$target_branch"`
+echo $status
+
+status=`execute git checkout $current_branch`
+echo $status
+
+echo "Zmiany wrzucone"
