57f601fe9c13a5dda9c0014e2546f949587a694a
Inscure <drimer.eco@gmail.com>
2014-10-10 23:38:01 +0200

    Showing the messages for executed commands

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ current_branch=`git rev-parse --abbrev-ref HEAD`
 
 
 
-function isUpToDate {
+function isUp2Date {
     a=`git rev-parse master`
     b=`git rev-parse origin/master`
 
@@ -30,8 +30,8 @@ status2=`$commands2`
 if [[ -z $status2 ]]; then
      echo "Wystąpił błąd"
 else
-    isUp2Date=$(isUpToDate)
-    if [ -z $isUp2Date ]; then
+    isUp2Date=$(isUp2Date)
+    if [ $isUp2Date == "0"  ]; then
         commands3="git push origin $current_branch"
         echo "$commands3"
         status3=`$commands3`
