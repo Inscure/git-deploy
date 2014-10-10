4ddd2dad43604cbdf4f0309b8fd176e5de2f0540
Inscure <drimer.eco@gmail.com>
2014-10-11 01:21:29 +0200

    Showing the messages for executed commands

--- merge.sh
+++ merge.sh
@@ -6,7 +6,7 @@ execute "git checkout $target_branch"
 
 execute "git merge origin/$target_branch"
 
-execute "git merge origin/$source_branch"
+execute "git merge $source_branch"
 
 execute "git push origin $target_branch"
 
